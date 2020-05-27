0647fa0366ca76430abd60f8c8a48a3ca7e4b3c9
Faris Demir <fdemir1@etf.unsa.ba>
2020-05-27 14:01:31 +0200

    Omogućen prikaz parking markera i info prozora

--- IMPLEMENTACIJA/Controllers/EParkingController.cs
+++ IMPLEMENTACIJA/Controllers/EParkingController.cs
@@ -17,6 +17,35 @@ namespace EParking.Controllers
         public IActionResult Map()
         {
             EParkingFacade.Instance.Parkinzi = _context.ParkingLokacija.ToList();
+            string markeri = "[";
+            int vel = 0;
+            foreach (ParkingLokacija parking in EParkingFacade.Instance.Parkinzi)
+            {
+                markeri += "{";
+                double lat = parking.Lat;
+                double lng = parking.Long;
+                markeri += String.Format("'lat': '{0}',", lat.ToString(System.Globalization.CultureInfo.InvariantCulture));
+                markeri += String.Format("'lng': '{0}',", lng.ToString(System.Globalization.CultureInfo.InvariantCulture));
+                //cijena po satu nije kako treba spasena u parking lokaciji u bazi pa zato ovo
+                //ISPRAVI TO KASNIJE!!!
+                double cijenaSat = 1.5;
+                markeri += string.Format("'naziv': '{0}',", parking.Naziv);
+                markeri += string.Format("'adresa': '{0}',", parking.Adresa);
+                markeri += string.Format("'cijena': '{0}',", cijenaSat);
+                markeri += string.Format("'slobodnaMjesta': '{0}',", parking.BrojSlobodnihMjesta);
+                markeri += string.Format("'kapacitet': '{0}'", parking.Kapacitet);
+                //markeri += "},";
+                if (vel < EParkingFacade.Instance.Parkinzi.Count - 1)
+                {
+                    markeri += "},";
+                }
+                else
+                {
+                    markeri += "}";
+                }
+            }
+            markeri += "];";
+            ViewBag.Markeri = markeri;
             return View(EParkingFacade.Instance);
         }
     }
