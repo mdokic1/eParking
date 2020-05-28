2703e64bc31896f613726ac869b8a1e12ff6aa3d
Faris Demir <fdemir1@etf.unsa.ba>
2020-05-28 18:43:39 +0200

    Slanje lokacije u kontroler EParking

--- IMPLEMENTACIJA/Controllers/EParkingController.cs
+++ IMPLEMENTACIJA/Controllers/EParkingController.cs
@@ -10,6 +10,8 @@ namespace EParking.Controllers
     public class EParkingController : Controller
     {
         private readonly EParkingContext _context;
+        ParkingLokacija odredisnaParkingLokacija = new ParkingLokacija();
+
         public EParkingController(EParkingContext context)
         {
             _context = context;
@@ -39,13 +41,11 @@ namespace EParking.Controllers
                 markeri += String.Format("'lng': '{0}',", lng.ToString(System.Globalization.CultureInfo.InvariantCulture));
                 //cijena po satu nije kako treba spasena u parking lokaciji u bazi pa zato ovo
                 //ISPRAVI TO KASNIJE!!!
-                //double cijenaSat = 1.5;
                 markeri += string.Format("'naziv': '{0}',", parking.Naziv);
                 markeri += string.Format("'adresa': '{0}',", parking.Adresa);
                 markeri += string.Format("'cijena': '{0}',", parking.Cjenovnik.DnevnaCijenaSat);
                 markeri += string.Format("'slobodnaMjesta': '{0}',", parking.BrojSlobodnihMjesta);
                 markeri += string.Format("'kapacitet': '{0}'", parking.Kapacitet);
-                //markeri += "},";
                 if (vel < EParkingFacade.Instance.Parkinzi.Count - 1)
                 {
                     markeri += "},";
@@ -54,6 +54,7 @@ namespace EParking.Controllers
                 {
                     markeri += "}";
                 }
+                vel++;
             }
             markeri += "];";
             ViewBag.Markeri = markeri;
@@ -61,10 +62,22 @@ namespace EParking.Controllers
             //A UKOLIKO JE GOST DA PRIKAZE NAJBLIZI PARKING OD NJEGOVE TRENUTNE LOKACIJE
             //true -- najblizi parking
             //false -- rezrvisani parking
-            ViewBag.NajbliziParking = "'true';";
+            ViewBag.NajbliziParking = "'false';";
             ViewBag.Latitude = EParkingFacade.Instance.Parkinzi.ElementAt(2).Lat.ToString(System.Globalization.CultureInfo.InvariantCulture);
             ViewBag.Longitude = EParkingFacade.Instance.Parkinzi.ElementAt(2).Long.ToString(System.Globalization.CultureInfo.InvariantCulture);
             return View(EParkingFacade.Instance);
+        }
+        [HttpPost]
+        public void SpasiKoordinate(double lat, double lon)
+        {
+            foreach (var p in EParkingFacade.Instance.Parkinzi)
+            {
+                if (p.Lat == lat && p.Long == lon)
+                {
+                    odredisnaParkingLokacija = p;
+                    break;
+                }
+            }
         }
     }
 }
